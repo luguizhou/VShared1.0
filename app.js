f8ce8ac34ba6c053f3ad01c573e84d3eb25fac8c
lugza <lugza@yonyou.com>
2015-07-30 09:56:00 +0800

    reconsitution 重构

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
-﻿var koa = require('koa')
+﻿var koa = require('koa'),
+path = require('path'),
+router = require('koa-router')(),
+routes = require('./routes/routes.js'),
+staticServer  = require('koa-static'),
+bodyparser = require('koa-bodyparser'),
+render = require('koa-swig');
 var app = koa();
-var path = require('path');
-var bodyparser = require('koa-bodyparser');
-var router = require('koa-router')();
-var routes = require('./routes/routes.js');
-
-var staticServer  = require('koa-static');
-var render = require('koa-swig');
-app.use(staticServer(__dirname + '/Content'));
+console.log(__dirname);
+app.use(staticServer(__dirname + '/public'));
 app.use(bodyparser());
 app.context.render = render({
     root: path.join(__dirname, 'views'),
     ext: 'html',
-    //locals: locals,
-    // filters: filters
-
 });
 
 routes(router);
-app.use(router.routes())
-    .use(router.allowedMethods());
+app.use(router.routes());
 if (module.parent) {
     module.exports = app.callback();
 } else {
--- routes/routes.js
+++ routes/routes.js
@@ -1,26 +1,24 @@
-﻿/*//var blog = require('../controllers/blog.js');
-var db = require('../redis-db/db');
-var doctors = require('../models/User');*/
-var parser = require("co-body");
+﻿var parser = require("co-body");
 //连接 redis 服务器
 var dbCo = require('../redis-db/db');
 module.exports = routes;
 function routes(router) {
     router.get('/', function*(next) {
-        yield this.render('home', {
-            user: {
-                name: 'fundon',
-                email: 'cfddream@gmail.com'
-            }
-        });
+        yield this.render("pc/home");
     });
-    router.get('/admin/:id/:test', function*(next) {
-        //debugger;
-        console.log(this.params);
-        console.log();
-        yield dbCo.hmset("hosts", "mjr", "1", "another", "23", "home", "1234","params",JSON.stringify( this.params));
-        this.body =yield dbCo.hgetall("hosts");
-        //this.body = JSON.parse(this.body);
-            console.log(this.body.params);
+    router.get('/login', function*(next) {
+        yield this.render("login");
     });
+    router.get('/register', function*(next) {
+        yield this.render("register");
+    });
+    router.post('/login', function*(next) {
+       this.body = {"username":this.params.username};
+    });
+    router.get('/admin', function*(next) {
+        yield this.render("admin/index");
+    });
+
+
+
 }
