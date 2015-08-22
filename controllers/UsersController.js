e7019cf815ed97e761688f9f528a84e54c4126c1
luguizhou <422386546@qq.com>
2015-08-22 16:37:27 +0800

    ron 的应用，便于大规模的开发

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,16 +1,7 @@
 ﻿var users = require('../models/User.js');
-exports.list = function (next) {
-    doctors.list({
-        direction: 'desc'
-    }, function (err, users) {
-        console.log(users);
-        if (!err) {
-            return users;
-        } else {
-            return console.log(err);
-        }
-    });
-
+exports.list = function* (params) {
+    var result = yield users.list(params);
+    return result;
 };
 
 exports.add = function *(params) {
@@ -18,78 +9,21 @@ exports.add = function *(params) {
 };
 
 exports.get = function *(params) {
-    return yield users.get({ id: params.id });;
+    return yield users.get({ id: params.id });
 };
 
-exports.update = function (req, res, next) {
-    doctors.update({
-        id: this.params.id,
-        username: this.params.username,
-        email: this.params.email,
-        password: this.params.password
-    }, function (err, user) {
-        return console.log(user);
-        if (!err) {
-            console.log("updated");
-        } else {
-            console.log(err);
-        }
-        return user;
-
-    });
+exports.update = function *(params) {
+    return yield users.update(params);
 };
 
-exports.delete = function (req, res, next) {
-    doctors.remove(
-        {
-            id: this.params.id
-        }
-    , function (err, user) {
-            if (!err) {
-                console.log("removed");
-                return "";
-            } else {
-                console.log(err);
-            }
-            return console.log(user);
-        });
-
+exports.userremove = function *(params) {
+    return yield users.userremove(params);
 };
 
-exports.login = function (req, res, next) {
-    doctors.get({ username: this.params.username, password: this.params.password }
-        , function (err, user) {
-        if (!err) {
-            console.log(user);
-            if (user) {
-                console.log({ ret: true, msg: '成功登陆', data: user });
-                return { ret: true, msg: '成功登陆', data: user };
-
-            }
-            else {
-                console.log({ ret: false, msg: '请检查用户名和密码' });
-                return { ret: false, msg: '请检查用户名和密码' };
-            }
-        }
-    });
+exports.login = function *(next) {
+    return yield users.login(params);
 };
 
-exports.usernameIsExist = function (req, res, next) {
-    doctors.get({ username: this.params.username }
-        , function (err, user) {
-        if (!err) {
-            console.log(user);
-            if (user) {
-                console.log({ ret: true, msg: '用户名已经存在，请修改用户名！', data: user });
-                return { ret: true, msg: '用户名已经存在，请修改用户名！', data: user };
-
-            }
-            else {
-                console.log({ ret: false, msg: '可以使用此用户名称' });
-                return { ret: false, msg: '可以使用此用户名称' };
-            }
-        }
-    });
-
-
+exports.usernameIsExist = function *(req, res, next) {
+    return yield users.usernameIsExist(params);
 }
